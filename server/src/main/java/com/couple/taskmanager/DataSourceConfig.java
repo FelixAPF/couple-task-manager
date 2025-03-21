2101ff7f4f6be3d5cdf889c2cfeafb3f9b8dcfb3
Felix Pelletier <felix.pelletierf@hotmail.com>
2025-03-20 22:54:34 -0400

    Add docker and initialize mobile version

--- server/src/main/java/com/couple/taskmanager/DataSourceConfig.java
+++ server/src/main/java/com/couple/taskmanager/DataSourceConfig.java
@@ -1,18 +1,29 @@
 package com.couple.taskmanager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+    @Autowired
+    private Environment environment;
+
     @Bean
     public DataSource dataSource() {
+        String password = environment.getProperty("DATABASE_PASSWORD");
+        String connectionString = "jdbc:sqlserver://couple-task-manager-active.database.windows.net:1433;" +
+                "database=couple-task-manager-active;" +
+                "user=CloudSA4f7cabff@couple-task-manager-active;" +
+                "password={" + password + "};" +
+                "encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         return DataSourceBuilder.create()
                 .driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
-                .url("jdbc:sqlserver://couple-task-manager-active.database.windows.net:1433;database=couple-task-manager-active;user=CloudSA4f7cabff@couple-task-manager-active;password={Halowars123!};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;")
+                .url(connectionString)
                 .build();
     }
 }

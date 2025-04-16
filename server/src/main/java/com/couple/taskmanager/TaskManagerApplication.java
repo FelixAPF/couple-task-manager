799b6c10a7435753d4296899fb74feff9286e31a
Felix Pelletier <felix.pelletierf@hotmail.com>
2025-04-15 21:18:16 -0400

    sync server timezone to est

--- server/src/main/java/com/couple/taskmanager/TaskManagerApplication.java
+++ server/src/main/java/com/couple/taskmanager/TaskManagerApplication.java
@@ -1,13 +1,22 @@
 package com.couple.taskmanager;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Montreal"));
 	}
 
 }
--- server/src/main/java/com/couple/taskmanager/service/VersionService.java
+++ server/src/main/java/com/couple/taskmanager/service/VersionService.java
@@ -7,9 +7,6 @@ import com.couple.taskmanager.repository.VersionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
 @Service
 public class VersionService {
     @Autowired

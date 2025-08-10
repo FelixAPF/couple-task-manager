fe382e5b62174edd1dbf8b72ec7f28ab0fa235e4
Felix Pelletier <felix.pelletierf@hotmail.com>
2025-08-10 19:01:02 -0400

    upgrade look for travel checklist

--- server/src/main/java/com/couple/taskmanager/repository/TripRepository.java
+++ server/src/main/java/com/couple/taskmanager/repository/TripRepository.java
@@ -2,12 +2,13 @@ package com.couple.taskmanager.repository;
 
 import com.couple.taskmanager.model.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
     List<Trip> findByUserIdOrderByDepartureDateDesc(Long userId);
 
-
+    @Query("UPDATE Trip t SET t.completed = :completed")
     void setTripCompletedEquals(boolean completed);
 }

fffde7902d9b82fa3fd0df6b6a98c32406dfc7c6
Felix Pelletier <felix.pelletierf@hotmail.com>
2026-02-12 08:09:14 -0500

    V2.30 minor bug fix for server error reload

--- client/src/app/server-error/server-error.component.ts
+++ client/src/app/server-error/server-error.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'; // Import RouterLink for the button
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router'; // Import RouterLink for the button
 import { NgIf } from '@angular/common'; // Import NgIf if you want conditional messages
 import { TranslatePipe } from '@ngx-translate/core';
+import { VersionControlService } from '../service/version-control.service';
+import { catchError, interval, of, Subscription, switchMap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-server-error',
@@ -10,13 +12,16 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './server-error.component.css',
   providers: [TranslatePipe]
 })
-export class ServerErrorComponent {
+export class ServerErrorComponent implements OnInit, OnDestroy {
+  router: Router = inject(Router);
+  versionService: VersionControlService = inject(VersionControlService);
   // You could pass error details via router state if needed,
   // but for a "server down" page, a static message is usually enough.
 
   // Simple property to control a message if you wanted to distinguish
   // between temporary outage vs. long-term maintenance.
   isMaintenanceMode: boolean = false;
+  subscription: Subscription = new Subscription();
 
   // You might inject a service here to report the error or check server status
   // private statusService = inject(StatusService);
@@ -25,4 +30,27 @@ export class ServerErrorComponent {
   reloadPage(): void {
     window.location.reload();
   }
+  
+  ngOnInit(){
+    const seconds = 5;
+    const retrievalIntervalMs = seconds * 1000;
+    this.subscription.add(interval(retrievalIntervalMs).pipe(
+      switchMap(() => this.versionService.retrieveVersion().pipe(
+      catchError(err => {
+            console.error('Error fetching data:', err);
+            // Return a new observable of an undefined value to keep the main stream alive
+            return of(undefined);
+          })
+      ))
+    ).subscribe((v) => {
+      if(v){
+        this.router.navigateByUrl("/dashboard");
+      }
+    }))
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  
 }

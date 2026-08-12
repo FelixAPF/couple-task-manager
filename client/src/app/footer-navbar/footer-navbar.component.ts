74f924363e5a7ce50f593928cdad00bea2e17cb3
Felix Pelletier <felix.pelletierf@hotmail.com>
2026-08-12 18:23:28 -0400

    V2.69 UI navbar revamp

--- client/src/app/footer-navbar/footer-navbar.component.ts
+++ client/src/app/footer-navbar/footer-navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { AppUpdate } from '@capawesome/capacitor-app-update';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../service/auth.service';
 import { SharedModule } from '../shared.module';
 import { HouseholdService } from '../service/household.service';
@@ -13,20 +14,26 @@ import { Household } from '../model/household';
   templateUrl: './footer-navbar.component.html',
   styleUrl: './footer-navbar.component.css'
 })
-export class FooterNavbarComponent implements OnInit {
+export class FooterNavbarComponent implements OnInit, OnDestroy {
   private authService = inject(AuthService);
   private householdService = inject(HouseholdService);
-  household: Household | null;
+  household: Household | null = null;
 
-  
-  ngOnInit(): void {
-    this.householdService.household$.subscribe((household) => {
-      this.household = household;
-    })
-  }
-
+  private subscription = new Subscription();
 
   isLoggedIn$ = this.authService.isLoggedIn$; // Make public for template access
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.householdService.household$.subscribe((household) => {
+        this.household = household;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   public async openAppStore(): Promise<void> {
     await AppUpdate.openAppStore();
